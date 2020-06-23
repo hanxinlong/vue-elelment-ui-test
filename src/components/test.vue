b11cd6ad24e2763716fcbd6119e1c1e42c4ad909
hanxinlong <hanxl@tuoxingwang.net>
2020-06-23 16:57:22 +0800

    初始化项目

---
+++ src/components/test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <el-button>点击</el-button>
+    <el-tag type="success">{{msg}}</el-tag>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      msg: 'Welcome to test!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
